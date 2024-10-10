6d16bcedfe755fc16b0a3692da6f747e720cf778
toralfs <toralfs97@gmail.com>
2024-10-10 16:52:30 +0200

    added category group names + some reformatting

--- cmd/fg-webcat-parser/main.go
+++ cmd/fg-webcat-parser/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
+// --------------------------- Consts -----------------------------
 const welcomeMessage string = `
 ---------------------------------------------------------
 Welcome to FG webcategory parser
@@ -19,53 +21,115 @@ and the program will extract the blocked categories.
 File path needs to be: ../../assets/fg-category-list.txt
 ---------------------------------------------------------
 `
-
 const inputMessage string = "Enter the configuration snippet and press enter."
+const blockedCategoriesMessage string = `
+---------------------------------------------------------
+Format is as below:
+Category Group
+    Category Name
 
+The blocked categories are: 
+---------------------------------------------------------
+`
+const exitMessage string = `
+---------------------------------------------------------
+Press enter to close the program
+`
+const filepath string = "../../assets/fg-category-list.txt"
+
+// --------------------------- Structs -----------------------------
+type fgCatGroup struct {
+	GrpID   string
+	GrpName string
+	Cats    map[int]fgCategory
+}
+
+type fgCategory struct {
+	CatID   int
+	CatName string
+}
+
+// --------------------------- Main -----------------------------
 func main() {
 	fmt.Print(welcomeMessage)
 	fmt.Println(inputMessage)
 	fgWebConf := readUserInput()
-	fgWebCats := readTextFile("../../assets/fg-category-list.txt")
+	fgWebCats := readTextFile(filepath)
 
 	cm := makeCategoryMap(fgWebCats)
 	bcIDs := findBlockedCategoryIDs(fgWebConf)
-	allBlockedCats := getBlockedCats(cm, bcIDs)
 
-	fmt.Println("---------------------------------------------------------")
-	fmt.Println("The blocked categories are: ")
-	fmt.Println("---------------------------------------------------------")
-	for _, catname := range allBlockedCats {
-		fmt.Println(catname)
-	}
+	fmt.Print(blockedCategoriesMessage)
 
-	fmt.Printf("\n Press enter to close the program \n")
+	printBlockedCats(cm, bcIDs)
+
+	fmt.Print(exitMessage)
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
 
-func getBlockedCats(fgCatMap map[int]string, blockedCats []int) []string {
-	var catnames []string
-	for _, cat := range blockedCats {
-		catnames = append(catnames, fgCatMap[cat])
-	}
-
-	return catnames
-}
-
-func makeCategoryMap(fgc []string) map[int]string {
-	m := make(map[int]string)
-
-	for _, cat := range fgc {
-		f := strings.SplitN(cat, " ", 2)
-
-		i, err := strconv.Atoi(f[0])
-		if err == nil {
-			m[i] = f[1]
-		} else {
-			fmt.Println("Failed to convert: ", i, "to int", err)
+// --------------------------- Functions -----------------------------
+func printBlockedCats(fgCatMap []fgCatGroup, blockedCats []int) {
+	for _, group := range fgCatMap {
+		var cNames []string
+		for _, cat := range blockedCats {
+			n, ok := group.Cats[cat]
+			if ok {
+				cNames = append(cNames, n.CatName)
+			}
+		}
+		if len(cNames) > 0 {
+			fmt.Println(group.GrpName)
+			for _, n := range cNames {
+				fmt.Println("    ", n)
+			}
 		}
 	}
-	return m
+}
+
+func makeCategoryMap(fgc []string) []fgCatGroup {
+	// Create a regex to match the "gXX Group Name" format
+	groupRegex := regexp.MustCompile(`^(g\d{2})\s+(.*):$`)
+
+	var groups []fgCatGroup
+	var currentGroup *fgCatGroup
+
+	for _, line := range fgc {
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue
+		}
+
+		if match := groupRegex.FindStringSubmatch(line); match != nil {
+			// If we have an existing group, save it before starting a new one
+			if currentGroup != nil {
+				groups = append(groups, *currentGroup)
+			}
+
+			// Create a new group with GrpID and GrpName
+			currentGroup = &fgCatGroup{
+				GrpID:   match[1],
+				GrpName: match[2],
+				Cats:    map[int]fgCategory{},
+			}
+		} else if currentGroup != nil {
+			fields := strings.Fields(line)
+			catID, _ := strconv.Atoi(fields[0])
+			catName := strings.Join(fields[1:], " ")
+			category := fgCategory{
+				CatID:   catID,
+				CatName: catName,
+			}
+
+			currentGroup.Cats[catID] = category
+		}
+	}
+
+	// Append the last group after exiting the loop
+	if currentGroup != nil {
+		groups = append(groups, *currentGroup)
+	}
+
+	return groups
 }
 
 func findBlockedCategoryIDs(fgWebConf []string) []int {
